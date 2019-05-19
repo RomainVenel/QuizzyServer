68dcf983929a8935e3a901feb7e7f79ffbf0c112
Mehdi <mehdi.hamidi.sio@gmail.com>
2019-05-19 19:54:47 +0200

    [friendlist][refactor] friend requests done

--- src/QuizzyBundle/Controller/UserController.php
+++ src/QuizzyBundle/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace QuizzyBundle\Controller;
 
+use QuizzyBundle\Entity\Friend;
 use QuizzyBundle\Service\FriendService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -22,7 +23,7 @@ class UserController extends Controller
     {
         $user = $this->em()->getRepository(User::REFERENCE)->find($user);
         $search = strtolower($request->request->get('search'));
-        $friendService  = $this->get(FriendService::REFERENCE);
+        $friendService = $this->get(FriendService::REFERENCE);
         $friends = $friendService->getFriendsByUser($user);
         $res = [];
 
@@ -30,7 +31,7 @@ class UserController extends Controller
             $friend = $friend->getUser()->getId() === $user->getId() ? $friend->getUserSender() : $friend->getUser();
             $name = strtolower($friend->getLastName() . ' ' . $friend->getFirstName());
 
-            if ($search != null && (substr_count($name, $search) === 0 && substr_count($friend->getEmail(), $search) === 0) ) {
+            if ($search != null && (substr_count($name, $search) === 0 && substr_count($friend->getEmail(), $search) === 0)) {
                 continue;
             }
 
@@ -58,13 +59,68 @@ class UserController extends Controller
     {
         $currentUser = $this->em()->getRepository(User::REFERENCE)->find($currentUser);
         $user = $this->em()->getRepository(User::REFERENCE)->find($user);
-        $friendService  = $this->get(FriendService::REFERENCE);
+        $friendService = $this->get(FriendService::REFERENCE);
         $friend = $friendService->getFriends($currentUser, $user);
 
         if ($friend) {
             $this->em()->remove($friend);
             $this->em()->flush();
         }
+        return new JsonResponse([], 200);
+    }
+
+    /**
+     * @Route("/friend/{user}/request")
+     * @param $user
+     * @return JsonResponse
+     */
+    public function getFriendsRequestAction($user)
+    {
+        $user = $this->em()->getRepository(User::REFERENCE)->find($user);
+        $friendService = $this->get(FriendService::REFERENCE);
+        $friendsRequest = $friendService->getFriendsRequestByUser($user);
+        $res = [];
+
+        foreach ($friendsRequest as $friend) {
+            $friend = $friend->getUserSender();
+            $res[] = [
+                "id" => $friend->getId(),
+                "firstName" => $friend->getFirstName(),
+                "lastName" => $friend->getLastName(),
+                "username" => $friend->getUsername(),
+                "birthDate" => ["year" => (int)$friend->getBirthDate()->format("Y"), "month" => (int)$friend->getBirthDate()->format("m"), "day" => (int)$friend->getBirthDate()->format("d")],
+                "password" => $friend->getPassword(),
+                "email" => $friend->getEmail(),
+                "media" => $friend->getMedia() ? $friend->getMedia()->getPath() : null,
+            ];
+        }
+
+        return new JsonResponse($res, 200);
+    }
+
+    /**
+     * @Route("/friend/request/{currentUser}/{userSender}/{choice}")
+     * @param $currentUser
+     * @param $userSender
+     * @param $choice
+     * @return JsonResponse
+     */
+    public function choiceFriendRequestAction($currentUser, $userSender, $choice)
+    {
+        $currentUser = $this->em()->getRepository(User::REFERENCE)->find($currentUser);
+        $userSender = $this->em()->getRepository(User::REFERENCE)->find($userSender);
+        $friendRequest = $this->em()->getRepository(Friend::REFERENCE)->findOneBy(['user_sender' => $userSender, 'user' => $currentUser]);
+
+        if ($friendRequest) {
+            if ($choice) {
+                $friendRequest->setAccepted(true);
+                $this->em()->persist($friendRequest);
+            } else {
+                $this->em()->remove($friendRequest);
+            }
+        }
+
+        $this->em()->flush();
         return new JsonResponse([], 200);
     }
 
